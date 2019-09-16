689bb3677e0b2f7d42f4e90855d9d13a9840abc1
Matt <mattsully01@gmail.com>
2019-09-16 00:38:04 -0600

    switching to materialize

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // DB Config
-const db = require('./config/keys').MongoURI;
+const db = require("./config/keys").MongoURI;
 
 // Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// serving public folder
+app.use(express.static('public'));
+// app.use(express.static(__dirname + "/styles"));
+// app.use(express.static(__dirname + "/js"));
 
 // Bodyparser
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Express Session
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true,
-  }));
+    saveUninitialized: true
+  })
+);
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -41,17 +49,16 @@ app.use(flash());
 
 // Global Vars
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, console.log("http://localhost:" + PORT,));
+app.listen(PORT, console.log("http://localhost:" + PORT));
