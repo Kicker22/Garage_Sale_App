b5ee3a968ceb5e557a5c125c44bd96232f3606e2
mworth <mikeworthen55@gmail.com>
2019-09-15 23:03:32 -0600

    hid key to MongoDB

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@ module.exports = {
     // Cloud URL
 
     // Compass DB URL
-    MongoURI: 'mongodb+srv://mworth55:WORTHsilver55@cluster0-7imit.mongodb.net/test'
+    // MongoURI: 'mongodb+srv://mworth55:WORTHsilver55@cluster0-7imit.mongodb.net/test'
 }
