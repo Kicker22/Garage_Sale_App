1e35c2f983334cf6b08c650c6daef29cee281216
Matt <mattsully01@gmail.com>
2019-09-18 00:06:11 -0600

    soma

--- routes/users.js
+++ routes/users.js
@@ -17,74 +17,6 @@ router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
-<<<<<<< HEAD
-  // Check required fields
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: "Please fill in all fields" });
-  }
-
-  // Check if passwords match
-  if (password !== password2) {
-    errors.push({ msg: "Passwords do not match" });
-  }
-
-  // Check password length
-  if (password.length < 6) {
-    errors.push({ msg: "Password should be at least 6 characters" });
-  }
-
-  if (errors.length > 0) {
-    res.render("register", {
-      errors,
-      name,
-      email,
-      password,
-      password2
-    });
-  } else {
-    // Validation passed
-    User.findOne({ email: email }).then(user => {
-      if (user) {
-        // User exists
-        errors.push({ msg: "Email is already registered" });
-        res.render("register", {
-          errors,
-          name,
-          email,
-          password,
-          password2
-        });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password
-        });
-
-        // Hash Password
-        bcrypt.genSalt(10, (err, salt) =>
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            // Set password to hashed
-            newUser.password = hash;
-            // Save user
-            newUser
-              .save()
-              .then(user => {
-                req.flash(
-                  "success_msg",
-                  "You are now registered and can log in"
-                );
-                res.redirect("/users/login");
-              })
-              .catch(err => console.log(err));
-          })
-        );
-        console.log(newUser);
-      }
-    });
-  }
-=======
     // Check required fields
     if (!name || !email || !password || !password2) {
         errors.push({ msg: 'Please fill in all fields' });
@@ -188,7 +120,6 @@ router.post("/register", (req, res) => {
 
             }
         });
->>>>>>> 0a8ff6a32a59d2e77c5c68dc26a02b0daf434845
 });
 
 // Login Handle
